ab9c123c6342723150ea170b991832943a98a464
haisedev <douglas.dallavale@acad.pucrs.br>
2021-05-26 20:12:16 -0300

    Model de curriculo

--- curricula-service/models/curriculumModel.js
+++ curricula-service/models/curriculumModel.js
@@ -1,1 +1,26 @@
-class CurriculumModel {}
+class CurriculumModel {
+    constructor(id, name, skills, courses) {
+        this.id = id;
+        this.name = name;
+        this.skills = skills;
+        this.courses = courses;
+    }
+
+    get id() {
+        return this.id;
+    }
+
+    get name() {
+        return this.name;
+    }
+
+    get skills() {
+        return this.skills;
+    }
+
+    get courses() {
+        return this.courses;
+    }
+}
+
+export default CurriculumModel;
