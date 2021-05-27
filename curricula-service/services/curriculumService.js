5c02fd011caeae8a881e34e9f73be88788a7d546
haisedev <douglas.dallavale@acad.pucrs.br>
2021-05-26 21:06:39 -0300

    Esqueleto do serviço de curriculos

---
+++ curricula-service/services/curriculumService.js
@@ -0,0 +1,25 @@
+
+/*
+/curricula
+/curricula/{{id}}
+/curricula/{{id}}/skills
+/skills
+/skills/{{id}}
+/curricula/{{id}}/courses
+*/
+
+class CurriculumService {
+
+    //curricula
+    list(){}
+
+    //curricula/:id
+    searchCurriculum(id){}
+
+    //curricula/:id/skills
+    getCurriculumSkils(id){}
+
+    //curricula/:id/courses
+    getCurriculumCourses(id){}
+
+}
