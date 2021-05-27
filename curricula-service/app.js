0f5d6179c576e6352003b5e940bbdf95b04375d2
haisedev <douglas.dallavale@acad.pucrs.br>
2021-05-26 21:05:51 -0300

    App singleton

--- curricula-service/app.js
+++ curricula-service/app.js
@@ -1,1 +1,28 @@
-class App{}
+import express from 'express';
+
+class App{
+    constructor(port = 5000){
+        this.port = port;
+        this.server = express();
+        this.includeMiddlewares();
+        this.start();
+    }
+
+    static instance() {
+        if (!App._instance) {
+          App._instance = new App();
+        }
+        return App._instance;
+    }
+
+    includeMiddlewares(){
+        this.server.use(express.json());
+        this.server.use(express.urlencoded({ extended: false }));
+    }
+
+    start() {
+        this.server.listen(this.port, () => console.log(`Server now listening at ${this.port}`));
+    }
+}
+
+const server = App.instance();
