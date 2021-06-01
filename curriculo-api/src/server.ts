13994698243458ef490ec9dc7205b40d0bcb09cb
haisedev <douglas.dallavale@acad.pucrs.br>
2021-06-01 20:01:58 -0300

    middleware de auth

--- curriculo-api/src/server.ts
+++ curriculo-api/src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { authorizer } from './middlewares/auth';
 
 const PORT = 4000;
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(authorizer);
 
 app.use('/api/v1', routes);
 
